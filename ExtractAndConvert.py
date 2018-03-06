ac4f3d7de745fbeedd17fdcd2b6710435b84af33
Jeroen Baten <jeroen@jeroenbaten.nl>
2018-03-06 15:52:12 +0100

    wip

--- ExtractAndConvert.py
+++ ExtractAndConvert.py
@@ -51,16 +51,6 @@ for link in soup.find_all('a'):
         page_to_convert_url= root_url + link  #  _parts[-1] # get last part after splitting to get page name
         print "page to request: ",page_to_convert_url
 
-        # todo get history of page and build a loop around it
-
-        # Actually retrieve wiki page
-        try:
-            r2 = requests.get(page_to_convert_url, allow_redirects=False) # to prevent redirection to https
-        except:
-            print "Unexpected error:", sys.exc_info()[0]
-        data2=r2.text
-        #print data2
-        #sys.exit(1)
         # Directory handling part
         # Check for directory
         destination_directory = export_root_dir + "/" + link_parts[-2]
@@ -71,14 +61,55 @@ for link in soup.find_all('a'):
             print "Not found! Creating!"
             os.makedirs(destination_directory)
 
-        # Try to remove everything after <!-- /patternContent-->
+
+        # todo get history of page and build a loop around it
+        filename = destination_directory + "/" + link_parts[-1] + ".original-html"
+        print "Filename: " + filename
+
+        if os.path.exists(filename):
+            # Load file
+            text_file = codecs.open(filename, "r", "utf-8")
+            data2=text_file.read()
+            text_file.close()
+            #file = open(“testfile.text”, “r”)
+            #print file.read()
+        else:
+            # Actually retrieve wiki page
+            try:
+                r2 = requests.get(page_to_convert_url, allow_redirects=False) # to prevent redirection to https
+            except:
+                print "Unexpected error:", sys.exc_info()[0]
+            data2=r2.text
+            #print data2
+            #sys.exit(1)
+
+            # save original html for checking purposes
+            # File handling part
+            # Save in libreplan.wiki/twiki
+            text_file = codecs.open(filename, "w", "utf-8")
+            text_file.write(data2)
+            text_file.close()
+
+
+
+        # Try to remove everything after <!-- /patternContent--> being the footer of the page.
+        # Remove everything after start of footer
+        # if 'span id=\"topic-actions\"' in data2:
+        #    data2=data2[0:data2.index('span id=\"topic-actions\"')]
+        # if 'http://twiki.org/?ref=twiki.org/topmenuskin' in data2:
+        #    data2=data2[0:data2.index('http://twiki.org/?ref=twiki.org/topmenuskin')]
         # soup2 = re.sub(r"\<\!\-\- \/patternContent\-\-\>.*\Z", "", soup2)
         # data2=data2[0:data2.index('span id=\"topic-actions\"')]
+        # [Edit](http://wiki.libreplan-enterprise.com/twiki/bin/edit/
+        # s/^\[Edit\](http\:\/\/wiki.libreplan-enterprise.com\/twiki\/bin\/edit\/.*$//'
+        # Copyright © by the contributing authors. All material on this collaboration platform is the property of the contributing authors.
+        # my_output = re.sub(r"^\[Edit\]\(http\:\/\/wiki.libreplan-enterprise.com\/twiki\/bin\/edit\/.*$", "", my_output)
+        # my_output = re.sub(r"^\[Edit\].*\Z", "", my_output)
         mysearch = "<!-- /patternContent-->"
         if mysearch in data2:
             data2 = data2[0:data2.index(mysearch)]
         #  </body> </html>
-        data2 = data2  + "</body> </html>"
+        data2 = data2 + "</body> </html>"
 
         # Change page using Beautifulsoup
         soup2=BeautifulSoup(data2, "html.parser")
@@ -87,18 +118,12 @@ for link in soup.find_all('a'):
 
         # save original html for checking purposes
         # File handling part
-        filename = destination_directory + "/" + link_parts[-1] + ".pre-html-processing"
-        print "Filename: " + filename
-        # Save in libreplan.wiki/twiki
-        text_file = codecs.open(filename, "w", "utf-8")
-        text_file.write(soup2.prettify())
-        text_file.close()
-
-        # Remove everything after start of footer
-        #if 'span id=\"topic-actions\"' in data2:
-        #    data2=data2[0:data2.index('span id=\"topic-actions\"')]
-        #if 'http://twiki.org/?ref=twiki.org/topmenuskin' in data2:
-        #    data2=data2[0:data2.index('http://twiki.org/?ref=twiki.org/topmenuskin')]
+        # filename = destination_directory + "/" + link_parts[-1] + ".pre-html-processing"
+        # print "Filename: " + filename
+        # # Save in libreplan.wiki/twiki
+        # text_file = codecs.open(filename, "w", "utf-8")
+        # text_file.write(soup2.prettify())
+        # text_file.close()
 
         # Remove some attributes
         REMOVE_ATTRIBUTES = [
@@ -151,19 +176,20 @@ for link in soup.find_all('a'):
 
 
         # Change urls
-        from_root="http://wiki.libreplan-enterprise.com/bin/view/"
-        for a in soup2.find_all("a","href"):
-            #print a
-            # to point to md files
-            #if from_root in a:
-            #if a.get("href") is not None:
-            a["href"] = a["href"]+".md"
-            # remove create new tag url
-            if "create new tag" in a:
-                a.decompose()
-            # remove view all tags url
-            if "view all tags" in a:
-                a.decompose()
+        # tmp disable
+        # from_root="http://wiki.libreplan-enterprise.com/bin/view/"
+        # for a in soup2.find_all("a","href"):
+        #     #print a
+        #     # to point to md files
+        #     #if from_root in a:
+        #     #if a.get("href") is not None:
+        #     a["href"] = a["href"]+".md"
+        #     # remove create new tag url
+        #     if "create new tag" in a:
+        #         a.decompose()
+        #     # remove view all tags url
+        #     if "view all tags" in a:
+        #         a.decompose()
 
         # Change img src="http://wiki.libreplan-enterprise.com/twiki/pub/TWiki/SmiliesPlugin/smile.gif
         #                 http://wiki.libreplan-enterprise.com/pub/
@@ -173,6 +199,7 @@ for link in soup.find_all('a'):
         #     if d["id"]=="patternBottomBar":
         #         d.decompose()
 
+        # no longer needed
         # span id="topic-actions
         # for s in soup2.find_all("span","topic-actions"):
         #     pprint(s)
@@ -195,12 +222,12 @@ for link in soup.find_all('a'):
 
         # save intermediate html for checking purposes
         # File handling part
-        filename=destination_directory+"/"+link_parts[-1] + ".pre-pandoc-concersion"
-        print "Filename: " + filename
-        # Save in libreplan.wiki/twiki
-        text_file = codecs.open(filename, "w", "utf-8")
-        text_file.write(soup2.prettify())
-        text_file.close()
+        # filename=destination_directory+"/"+link_parts[-1] + ".pre-pandoc-concersion"
+        # print "Filename: " + filename
+        # # Save in libreplan.wiki/twiki
+        # text_file = codecs.open(filename, "w", "utf-8")
+        # text_file.write(soup2.prettify())
+        # text_file.close()
 
         # Convert page to markdown_github syntax
         print "Start pandoc conversion..."
@@ -211,33 +238,56 @@ for link in soup.find_all('a'):
         pprint(my_output)
         print "*" * 80
 
+        # After pandoc conversion save intermediate file
+        filename=destination_directory+"/"+link_parts[-1] + ".raw-md"
+        print "Filename: " + filename
+        # Save in libreplan.wiki/twiki
+        text_file = codecs.open(filename, "w", "utf-8")
+        text_file.write(my_output)
+        text_file.close()
+        #output = pypandoc.convert_text(soup2, "markdown_github", format='html', outputfile=filename)
+        # assert output == ""
+        # todo: add to git
+
         # replace strings in md output
         # (/bin/view/ => (twiki/
-        my_output = my_output.replace("(/bin/view/", "(twiki/")
+        #my_output = my_output.replace("(/bin/view/", "(twiki/")
 
         # Next is not possible because also gifs in original
         #output = output.replace(")", ".md)")
 
         # change img urls to local
         #my_output = my_output.replace("http://wiki.libreplan-enterprise.com/twiki/pub/TWiki/TWikiDocGraphics/", "twiki/TWiki/TWikiDocGraphics/")
-        my_output = my_output.replace("http://wiki.libreplan-enterprise.com/twiki/pub/", "/twiki/")
+        #my_output = my_output.replace("http://wiki.libreplan-enterprise.com/twiki/pub/", "/twiki/")
 
         # Also replace all urls with "pub" in them
-        my_output = my_output.replace("/twiki/pub/TWiki/", "/twiki/TWiki/")
+        #my_output = my_output.replace("/twiki/pub/TWiki/", "/twiki/TWiki/")
 
         # /twiki/bin/view/
-        my_output = my_output.replace("/twiki/bin/view/", "/twiki/")
+        #my_output = my_output.replace("/twiki/bin/view/", "/twiki/")
 
         # remove persistent span in top of page
-        my_output = re.sub(r"<span id=\".*\">.*<\/span>", "", my_output)
+        #my_output = re.sub(r"<span id=\".*\">.*<\/span>", "", my_output)
 
 
-        # remove complete footer part from edit button to end of file!
-        # [Edit](http://wiki.libreplan-enterprise.com/twiki/bin/edit/
-        # s/^\[Edit\](http\:\/\/wiki.libreplan-enterprise.com\/twiki\/bin\/edit\/.*$//'
-        # Copyright © by the contributing authors. All material on this collaboration platform is the property of the contributing authors.
-        #my_output = re.sub(r"^\[Edit\]\(http\:\/\/wiki.libreplan-enterprise.com\/twiki\/bin\/edit\/.*$", "", my_output)
-        #my_output = re.sub(r"^\[Edit\].*\Z", "", my_output)
+        # Remove complete footer part from edit button to end of file!
+        # Now doing this earlier in original html
+
+        # Change url's in md phase
+        # To create a reference link, use two sets of square brackets.
+        # [[my internal link|internal-ref]] will link to the internal reference internal-ref.
+        # examples
+        # from: [url](/twiki/bin/view/Main/WebHome)
+        # to:   [[url|/twiki/Main/WebHome]]
+        #my_output = my_output.replace("/twiki/bin/view/", "/twiki/")
+        #my_output="[url](/twiki/bin/view/Main/WebHome)  and image [](/twiki/pub/TWiki/TWikiDocGraphics/web-bg-small.gif)"
+        my_output = re.sub(r"\[(.*?)\]\(/twiki/bin/view/(.*?)\)",
+                           r"[[\1|/twiki/\2]]\n",
+                           my_output)
+        # and image [](/twiki/pub/TWiki/TWikiDocGraphics/web-bg-small.gif)
+        my_output = re.sub(r"\[\]\(/twiki/pub/TWiki/TWikiDocGraphics/(.*?)\)",
+                           r"[[|/twiki/TWiki/TWikiDocGraphics/\1]]\n",
+                           my_output)
 
         # File handling part
         filename=destination_directory+"/"+link_parts[-1] + ".md"
